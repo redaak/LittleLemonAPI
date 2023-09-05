b0e54efddb1eef83b8d5dd5f92f849278fe645b8
Reda Akdim <redaakdim72@gmail.com>
2023-09-05 17:40:37 +0100

    first migration

--- littlelemonapi/settings.py
+++ littlelemonapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "mainAPI",
     "debug_toolbar",
-    "rest_framework"
+    "rest_framework",
 
 ]
 
--- mainAPI/views.py
+++ mainAPI/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .serializers import MenuItemSerializer
 from .models import MenuItem
 # Create your views here.
-class AllMenuItems(generics.CreateAPIView):
+class AllMenuItems(generics.ListCreateAPIView):
     queryset=MenuItem.objects.all()
     serializer_class=MenuItemSerializer
 
