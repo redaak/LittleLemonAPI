a19f3f7b44743bbf44b4caad4b9ee9dea75db8d7
Reda Akdim <redaakdim72@gmail.com>
2023-09-06 20:11:23 +0100

    added model to admin panel

--- mainAPI/serializers.py
+++ mainAPI/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import MenuItem
-
+from decimal import Decimal
 class MenuItemSerializer(serializers.ModelSerializer):
+    price_after_tax=serializers.SerializerMethodField(method_name="calculate_tax")
     class Meta:
         model=MenuItem
-        fields=["id","title","price","inventory"]
+        fields=["id","title","inventory","price","price_after_tax"]
+    def calculate_tax(self,product:MenuItem):
+        return product.price*Decimal(1.5)
